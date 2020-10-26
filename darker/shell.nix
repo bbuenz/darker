ffe44ab3d437caa3124425ed3570a8dd4bee9ae4
Philippe Camacho <philippe@findora.org>
2020-10-26 18:59:24 -0300

    Nix expression for installing LaTeX.

---
+++ darker/shell.nix
@@ -0,0 +1,37 @@
+with import <nixpkgs> { };
+
+let
+ tl = (texlive.combine {
+   inherit (texlive) scheme-small
+   collectbox
+   multirow
+   titlesec
+   soul
+   realboxes
+   cryptocode
+   xargs
+   forloop
+   pbox
+   varwidth
+   bigfoot
+   environ
+   trimspaces
+   comment
+   mdframed
+   zref
+   needspace
+   enumitem
+   ucs
+   todonotes
+   cleveref
+   ; });
+in mkShell {
+  buildInputs = [
+      gnumake
+      ghostscript
+      tl
+  ];
+  shellHook = ''
+    export PATH="$(pwd)/bin:$PATH"
+  '';
+}
